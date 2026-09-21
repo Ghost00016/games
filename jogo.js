3b4ec13021715e940a711391a60e98f0b0355feb
Lost <110736815+Ghost00016@users.noreply.github.com>
2026-09-21 14:42:36 -0300

    Add files via upload

---
+++ jogo.js
@@ -0,0 +1,176 @@
+const JOGOS = {
+    "aceattorney": {
+        nome: "Phoenix Wright: Ace Attorney",
+        url: "Jogos/ace-attorney/aceattorney/Phoenix Wright - Ace Attorney (USA).zip",
+        core: "nds",
+        partes: 2
+    },
+
+    "justiceforall": {
+        nome: "Phoenix Wright: Ace Attorney - Justice for All",
+        url: "Jogos/ace-attorney/Phoenix Wright - Ace Attorney - Justice for All (USA)/Phoenix Wright - Ace Attorney - Justice for All (USA).zip",
+        core: "nds",
+        partes: 1
+    },
+
+    "trialsandtribulations": {
+        nome: "Phoenix Wright: Ace Attorney - Trials and Tribulations",
+        url: "Jogos/ace-attorney/Phoenix Wright - Ace Attorney - Trials and Tribulations (USA)/Phoenix Wright - Ace Attorney - Trials and Tribulations (USA).zip",
+        core: "nds",
+        partes: 1
+    },
+
+    "apollojustice": {
+        nome: "Apollo Justice: Ace Attorney",
+        url: "Jogos/ace-attorney/apollojustice/Apollo Justice - Ace Attorney (USA).zip",
+        core: "nds",
+        partes: 3
+    },
+
+    "milesedgeworth": {
+        nome: "Ace Attorney Investigations: Miles Edgeworth",
+        url: "Jogos/ace-attorney/milesedgeworth/Ace Attorney Investigations - Miles Edgeworth (USA).zip",
+        core: "nds",
+        partes: 2
+    },
+
+    "pokemon-emerald": {
+        nome: "Pokémon Emerald",
+        url: "Jogos/Game-Boy-Advance/Pokemon - Emerald Version/Pokemon - Emerald Version.zip",
+        core: "gba",
+        partes: 10
+    },
+
+    "pokemon-fire-red-leaf-green": {
+        nome: "Pokémon FireRed & LeafGreen",
+        url: "Jogos/Game-Boy-Advance/Pokemon - Leaf Green Version & Fire Red Version/Pokemon - Leaf Green Version & Fire Red Version.zip",
+        core: "gba",
+        partes: 17
+    },
+
+    "final-fantasy-dawn-of-souls": {
+        nome: "Final Fantasy I & II: Dawn of Souls",
+        url: "Jogos/Game-Boy-Advance/Final Fantasy I & II - Dawn of Souls/Final Fantasy I & II - Dawn of Souls.zip",
+        core: "gba",
+        partes: 4
+    },
+
+    "mario-luigi-superstar-saga": {
+        nome: "Mario & Luigi: Superstar Saga",
+        url: "Jogos/Game-Boy-Advance/Mario & Luigi - Superstar Saga/Mario & Luigi - Superstar Saga.zip",
+        core: "gba",
+        partes: 5
+    },
+
+    "mario-golf-advance-tour": {
+        nome: "Mario Golf: Advance Tour",
+        url: "Jogos/Game-Boy-Advance/Mario Golf - Advance Tour/Mario Golf - Advance Tour.zip",
+        core: "gba",
+        partes: 4
+    },
+
+    "mario-kart-super-circuit": {
+        nome: "Mario Kart: Super Circuit",
+        url: "Jogos/Game-Boy-Advance/Mario Kart Super Circuit/Mario Kart Super Circuit.zip",
+        core: "gba",
+        partes: 2
+    },
+
+    "mario-vs-donkey-kong": {
+        nome: "Mario vs. Donkey Kong",
+        url: "Jogos/Game-Boy-Advance/Mario vs. Donkey Kong/Mario vs. Donkey Kong.zip",
+        core: "gba",
+        partes: 2
+    },
+
+    "sonic-advance": {
+        nome: "Sonic Advance",
+        url: "Jogos/Game-Boy-Advance/Sonic Advance/Sonic Advance.zip",
+        core: "gba",
+        partes: 1
+    },
+
+    "sonic-advance-2": {
+        nome: "Sonic Advance 2",
+        url: "Jogos/Game-Boy-Advance/Sonic Advance 2/Sonic Advance 2.zip",
+        core: "gba",
+        partes: 2
+    },
+
+    "sonic-advance-3": {
+        nome: "Sonic Advance 3",
+        url: "Jogos/Game-Boy-Advance/Sonic Advance 3/Sonic Advance 3.zip",
+        core: "gba",
+        partes: 2
+    },
+
+    "super-mario-advance": {
+        nome: "Super Mario Advance",
+        url: "Jogos/Game-Boy-Advance/Super Mario Advance/Super Mario Advance.zip",
+        core: "gba",
+        partes: 3
+    },
+
+    "super-mario-advance-2": {
+        nome: "Super Mario Advance 2: Super Mario World",
+        url: "Jogos/Game-Boy-Advance/Super Mario Advance 2 - Super Mario World/Super Mario Advance 2 - Super Mario World.zip",
+        core: "gba",
+        partes: 3
+    },
+
+    "super-mario-advance-3": {
+        nome: "Super Mario Advance 3: Yoshi's Island",
+        url: "Jogos/Game-Boy-Advance/Super Mario Advance 3 - Yoshi's Island/Super Mario Advance 3 - Yoshi's Island.zip",
+        core: "gba",
+        partes: 2
+    },
+
+    "super-mario-advance-4": {
+        nome: "Super Mario Advance 4: Super Mario Bros. 3",
+        url: "Jogos/Game-Boy-Advance/Super Mario Advance 4 - Super Mario Bros. 3/Super Mario Advance 4 - Super Mario Bros. 3.zip",
+        core: "gba",
+        partes: 3
+    },
+
+    "tomodachi-collection": {
+        nome: "Tomodachi Collection",
+        url: "Jogos/Nintendo/tomodachi-collection/tomodashi-collection-english.7z",
+        core: "nds",
+        partes: 2
+    },
+
+    "warioware-touched": {
+        nome: "WarioWare: Touched!",
+        url: "Jogos/Nintendo/warioware-touched/ww.zip",
+        core: "nds",
+        partes: 2
+    },
+
+    "nfscarbon": {
+        nome: "Need for Speed: Carbon",
+        url: "Jogos/need-for-speed/nfscabron/nfscarbon.zip",
+        core: "gba",
+        partes: 2
+    },
+
+    "nfsmostwanted": {
+        nome: "Need for Speed: Most Wanted",
+        url: "Jogos/need-for-speed/nfsmostwanted/nfsmostwanted.zip",
+        core: "gba",
+        partes: 1
+    },
+
+    "nfsunderground2": {
+        nome: "Need for Speed: Underground 2",
+        url: "Jogos/need-for-speed/nfsnderground2/nfsunderground2.zip",
+        core: "gba",
+        partes: 1
+    },
+
+    "san-andreas": {
+        nome: "Grand Theft Auto: San Andreas",
+        url: "Jogos/san-andreas/san-andreas.iso",
+        core: "psx",
+        partes: 217
+    }
+};
